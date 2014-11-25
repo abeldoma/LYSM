4bdf29624f5ad96f169845c438670b01f4a28765
vagrant <vagrant@ironhack>
2014-11-25 12:17:53 +0000

    'resources :users' route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 root 'site#home'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
