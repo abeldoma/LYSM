4244c85dbbdc9cb885463938255aae2c382f0bec
vagrant <vagrant@ironhack>
2014-12-01 16:53:21 +0000

    new video view

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,8 +2,8 @@ class Video < ActiveRecord::Base
 	belongs_to :band
 
 	validates :title, presence: true, length: {maximum: 35}
-	VALID_YOUTUBE_URL_REGEX = "^(http\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$"
-	validates :youtube_url, presence: true, format: { with: VALID_YOUTUBE_URL_REGEX }
+	yt_regexp = /http:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]*)/
+	validates :youtube_url, presence: true, format: { with: yt_regexp }
 
 
 end
